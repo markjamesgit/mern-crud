ed981b2ef050a204907aeff5417eb52a0983e74f
Mark James <142849183+markjamesgit@users.noreply.github.com>
2025-06-13 13:40:33 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+import productRoutes from './routes/product.route.js'
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(express.json()) //allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes)
+
+console.log(process.env.MONGO_URI)
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:" + PORT);
+})
---
+++ frontend/src/store/product.js
@@ -0,0 +1,21 @@
+import {create} from "zustand"
+
+export const useProductStore = create((set) => ({
+    products: [],
+    setProducts: (products) => set({products}),
+    createProduct: async(newProduct) => {
+        if(!newProduct.name || !newProduct.price || !newProduct.image){
+            return {succes:false, message:"Please fill in all fields."}
+        }
+        const res = await fetch("/api/products", {
+            method:"POST",
+            headers:{
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(newProduct)
+        })
+        const data = await res.json();
+        set((state) => ({products:[...state.products, data.data]}))
+        return {succes:true, message:"Product created successfully!"}
+    }
+}))
